fc73001f969b4de98543bdc376baa483793549dd
davydka <davydka@gmail.com>
2016-10-25 10:19:36 -0400

    Getting to work on formatting.

--- app.jsx
+++ app.jsx
@@ -3,17 +3,23 @@ var Resume = require('json!./resume.json');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Title = require('./components/title');
+var Contact = require('./components/contact');
 
 var App = React.createClass({
 
 	render: function(){
 		console.log(this.props);
-		return <Title
-			name={this.props.Resume.basics.label}
+		return (<div className='container'>
+			<Title
+			name={this.props.Resume.basics.name}
 			label={this.props.Resume.basics.label}
-			/>;
+			/>
+			<Contact
+			/>
+
+		</div>);
 	}
 });
 
 var element = React.createElement(App, {Resume});
-ReactDOM.render(element, document.querySelector('.container'));
+ReactDOM.render(element, document.querySelector('#root'));
---
+++ components/contact.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function(){
+		return <div className='contact'>
+			<div className='col-1'>
+				<h2>Contact</h2>
+			</div>
+			<div className='col-2'>
+				Email: davydka@gmail.com <br/>
+				Phone: (646) 209-4244 <br/>
+				Website: http://107.170.100.207:1981/ <br/>
+			</div>
+		</div>;
+	}
+})
